e418905f72c753ccdff600983663b55680685c94
Bartłomiej Wajda <bartek.rofl@gmail.com>
2016-11-24 09:16:27 +0100

    sorting

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new GameLibraryBundle\GameLibraryBundle(),
-            new FOS\UserBundle\FOSUserBundle()
+            new FOS\UserBundle\FOSUserBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
